ef97b50757d21c0f090d1bf93a47af3b4a0817a1
Muhammad Shoaib Riaz <shoaib.riaz@arbisoft.com>
2019-12-01 10:52:06 +0500

    data formation

--- front-end-mobile/App/Components/food-item/index.js
+++ front-end-mobile/App/Components/food-item/index.js
@@ -12,8 +12,23 @@ import {
 
 import * as styles from './foodItems.styles';
 import { Text } from '../text';
+import { assets } from '../../Assets';
+
+const IMG_DIC = {
+    0: assets.burger,
+    1: assets.biryani,
+    2: assets.pizza,
+    3: assets.sandwich
+}
 
 export const FoodItem = (props) => {
+    const {
+        itemId,
+        itemName,
+        itemPrice,
+        itemCategory
+    } = props.item;
+
     return (
         <TouchableOpacity
             style={styles.CONTAINER}
@@ -21,10 +36,14 @@ export const FoodItem = (props) => {
             onPress={props.onPress}
         >
             <ImageBackground
-                source={{ uri: 'https://www.qsrmagazine.com/sites/default/files/styles/story_page/public/phut_0.jpg?itok=Bzo3z1cD' }}
+                source={IMG_DIC[props.index % 4]}
                 style={styles.IMAGE}
             />
-            <Text perset="secondary" text={props.item} />
+            <Text perset="secondary" text={itemName} />
+            <Text perset="secondary" text={itemPrice} />
+            <Text perset="secondary" text={itemCategory} />
+
+
         </TouchableOpacity>
     );
 }
